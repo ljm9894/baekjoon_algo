4cf0d54a1b3b3ca6889d9b463963a0b28cb7617d
재명 <ljm9894@dgsw.hs.kr>
2023-06-12 00:17:58 +0900

    20230612 00 : 17

---
+++ src/bronze/_1453/Main.java
@@ -0,0 +1,22 @@
+package bronze._1453;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        int cnt=0;
+        boolean[] arr = new boolean[101];
+        n = sc.nextInt();
+        for(int i=1;i<=n;i++){
+            int req = sc.nextInt();
+            if(arr[req] == true){
+                cnt++;
+            }else{
+                arr[req] = true;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
