c13dbd1e2e0e7eb1d3f149070bc74d6c3bbb74ba
재명 <ljm9894@dgsw.hs.kr>
2023-10-25 23:49:33 +0900

    20231025 11:49

---
+++ src/bronze/_2309/Main.java
@@ -0,0 +1,30 @@
+package bronze._2309;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        int arr[] = new int[9];
+        for(int i=0;i<9;i++) {
+            arr[i] = sc.nextInt();
+            sum += arr[i];
+        }
+        for(int i=0;i<8;i++){
+            for(int j=i+1;j<9;j++){
+                if(sum- arr[i]- arr[j] == 100){
+                    arr[i] = 0;
+                    arr[j] = 0;
+                    Arrays.sort(arr);
+
+                    for(int k=2;k<9;k++){
+                        System.out.println(arr[k]);
+                    }
+                    return;
+                }
+            }
+        }
+    }
+}
