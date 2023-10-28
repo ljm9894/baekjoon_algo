595acf533d323487ecd6a6798b878dde901957ef
재명 <ljm9894@dgsw.hs.kr>
2023-10-29 05:04:04 +0900

    9 5:3

---
+++ src/silver/_2822/Main.java
@@ -0,0 +1,31 @@
+package silver._2822;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[151];
+        int cnt = 5;
+        int sum =0;
+        for(int i=1;i<=8;i++){
+            int k = sc.nextInt();
+            arr[k] = i;
+        }
+        List<Integer> num = new ArrayList<>();
+        for(int i=150;i>0;i--){
+            if(arr[i]!=0){
+                sum+=i;
+                cnt--;
+                num.add(arr[i]);
+            }
+            if(cnt==0) break;
+        }
+        Collections.sort(num);
+        System.out.println(sum);
+        for(int i=0;i<5;i++){
+            System.out.printf("%d ",num.get(i));
+        }
+    }
+}
