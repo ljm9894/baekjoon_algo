3f69f08aeee7b8df72af8be9407f305d1eae3584
재명 <ljm9894@dgsw.hs.kr>
2023-11-13 23:48:53 +0900

    20231113 23:48

---
+++ src/silver/_1094/Main.java
@@ -0,0 +1,24 @@
+package silver._1094;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine());
+        int stick = 64;
+        int cnt =0;
+
+        while(n>0){
+            if(n<stick){
+                stick/=2;
+            }else {
+                n -= stick;
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
