3caeefda94a0f3b81cb34df252099c3359fcb888
재명 <ljm9894@dgsw.hs.kr>
2023-11-18 23:50:49 +0900

    20231118 23:50

---
+++ src/bronze/b3/_1547/Main.java
@@ -0,0 +1,21 @@
+package bronze.b3._1547;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int M = sc.nextInt();
+        int ball = 1;
+        for(int i=0;i<M;i++){
+            int a =sc.nextInt();
+            int b = sc.nextInt();
+            if(ball == b){
+             ball = a;
+            }else if(ball == a){
+                ball = b;
+            }
+        }
+        System.out.println(ball);
+    }
+}
