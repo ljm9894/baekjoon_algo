55c967dc76a18c0d3f9cbe76632c155580762b53
재명 <ljm9894@dgsw.hs.kr>
2023-11-30 04:07:06 +0900

    20231130 04:07

---
+++ src/bronze/B5/_5338/Main.java
@@ -0,0 +1,11 @@
+package bronze.B5._5338;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("       _.-;;-._");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-;;-._|");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-''-._|");
+    }
+}
