7a28f911695c268b7b9f650b6d83d22be7f4cf2e
재명 <ljm9894@dgsw.hs.kr>
2023-12-01 01:50:02 +0900

    20231201 01:49

---
+++ src/bronze/B4/_3046/Main.java
@@ -0,0 +1,13 @@
+package bronze.B4._3046;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int R1 = sc.nextInt();
+        int S = sc.nextInt();
+        int result = S*2-R1;
+        System.out.println(result);
+    }
+}
